39076f5375149ab203f98618073053b750617926
Nima Mahmoudi <nima_mahmoudi@live.com>
2019-03-19 16:55:05 -0600

    added new method of getting dependencies

--- n1_fetch_all_deps/main.py
+++ n1_fetch_all_deps/main.py
@@ -1,95 +1,96 @@
-from xmlrpc import client as xmlrpclib
-# only one api server so we'll use the mirror for downloading
-client = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
-packages = client.list_packages()
+import numpy as np
+import pandas as pd
 
-import tarfile, re, requests, csv, json
-from base64 import b64encode
-import os
+
+data_folder = 'data/'
+
+df_dep = pd.read_csv(data_folder + 'pypi_dependencies.csv', error_bad_lines=False, warn_bad_lines=False)
+df_proj = pd.read_csv(data_folder + 'pypi_projects.csv', error_bad_lines=False, warn_bad_lines=False,low_memory=False)
+df_ver = pd.read_csv(data_folder + 'pypi_versions.csv', error_bad_lines=False, warn_bad_lines=False,low_memory=False)
+
+for df in [df_proj, df_ver, df_dep]:
+    time_fields = []
+    for col in df.columns:
+        if 'timestamp' in col:
+            time_fields.append(col)
+    for time_field in time_fields:
+        try:
+            df[time_field] = df[time_field].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+        except:
+            pass
+        
+del df
+
+print(df_dep.shape)
+print(df_proj.shape)
+print(df_ver.shape)
+
+new_deps = {}
+for col in df_dep.columns:
+    new_deps[col] = []
+del new_deps['id']
+print(new_deps)
+
 import time
+from xmlrpc import client as xmlrpclib
 
-tmp_tar = 'tmp/temp_tar'
+# I need this to separate the package name from its version
+client = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
+def get_dependencies(package_name, release_version):
+    rdata = client.release_data(package_name, release_version)
+    deps = []
+    dep_reqs = []    
+    if 'requires_dist' in rdata:
+        for req in rdata['requires_dist']:
+            a = req.split(';')
+            a = a[0]
+            b = a.split(' ')
+            c = b[0]
+            d = '*'
+            d = d.strip('()')
+            if len(b) > 1:
+                if len(b[1]) > 0:
+                    d = b[1]
+            deps.append(c)
+            dep_reqs.append(d)
+            
+    time.sleep(.18)
+    return deps, dep_reqs
 
-def ensure_dir(path):
-    try:
-        dir_name = os.path.dirname(path)
-        os.makedirs(dir_name, exist_ok=True)  # succeeds even if directory exists.
-    except:
-        os.makedirs(path, exist_ok=True)  # succeeds even if directory exists.
-
-
-def _save_file(pathname, member, tar_file):
-    try:
-        content = tar_file.extractfile(member).read()
-    except:
-        return
-
-    outfilename = '{}{}'.format(pathname, os.path.basename(member.name))
-    ensure_dir(outfilename)
-    with open(outfilename, 'wb') as outfile:
-        outfile.write(content)
-    return
-
-
-def _extract_files(package_file, name):
-    try:
-        tar_file = tarfile.open(fileobj=package_file)
-    except:
-        return
-    for member in tar_file.getmembers():
-        if 'setup.py' in member.name or 'requirements' in member.name:
-            _save_file(name, member, tar_file)
-        #    content = tar_file.extractfile(member).read()
-        #    with open('{}{}'.format(name, os.path.basename(member.name)), 'w') as outfile:
-        #        outfile.write(content)
-        #elif 'requirements' in member.name:
-        #    content = tar_file.extractfile(member).read()
-        #    with open('{}{}'.format(name, os.path.basename(member.name)), 'w') as outfile:
-        #        outfile.write(content)
+package_name = 'django'
+release_version = '2.1.7'
+get_dependencies(package_name, release_version)
 
 
-def extract_package(name, client=xmlrpclib.ServerProxy('http://pypi.python.org/pypi')):
-    for release in client.package_releases(name):
-        outdir = 'packages/{}-{}/'.format(name, release)
-        doc = client.release_urls(name, release)
-        if doc:
-            url = None
-            for d in doc:
-                if d['python_version'] == 'source' and d['url'].endswith('gz'):
-                    url = d['url']
-            if url:
-                #print(doc[3])
-                #url = doc[0].get('url')#.replace("http://pypi.python.org/", "http://f.pypi.python.org/")
-                #print "Downloading url %s" % url
-                req = requests.get(url)
-                if req.status_code != 200:
-                    print("Could not download file %s" % req.status_code)
-                else:
-                    #print(outdir)
-                    ensure_dir('{}'.format(outdir))
-                    with open(tmp_tar, 'wb') as tar_file:
-                        tar_file.write(req.content)
-                    with open(tmp_tar, 'rb') as tar_file:
-                        return _extract_files(tar_file, name=outdir)
 
-def robust_extract(package, client):
-    try:
-        extract_package(package, client)
-    except requests.exceptions.HTTPError as e:
-        # Whoops it wasn't a 200
-        print("Error: " + str(e))
-        time.sleep(10)
-        robust_extract(package, client)
+count = 0
+for index, row in df_ver.iterrows():
+    count += 1
+    if count > 100:
+        break
 
-if __name__=='__main__':
-    ensure_dir('packages')
-    ensure_dir('tmp')
+    package_name = df_ver.loc[index, 'project_name']
+    release_version = df_ver.loc[index, 'number']
+    print(package_name, release_version)
+    deps, dep_reqs = get_dependencies(package_name, release_version)
+    dep_counts = len(deps)
+    
+    new_deps['platform'] += (['Pypi']*dep_counts)
+    new_deps['project_name'] += ([package_name]*dep_counts)
+    new_deps['project_id'] += ([df_ver.loc[index, 'project_id']]*dep_counts)
+    new_deps['version_number'] += ([release_version]*dep_counts)
+    new_deps['version_id'] += ([df_ver.loc[index, 'id']]*dep_counts)
+    new_deps['dependency_name'] += deps
+    new_deps['dependency_platform'] += (['Pypi']*dep_counts)
+    new_deps['dependency_kind'] += (['runtime']*dep_counts)
+    new_deps['optional_dependency'] += ([False]*dep_counts)
+    new_deps['dependency_requirements'] += dep_reqs
+    new_deps['dependency_project_id'] += ([np.nan]*dep_counts)
 
-    print('starting...')
-    for i, package in enumerate(packages[118300+4100:]):
-        if i % 100 == 0:
-            print('Extracting package {} / {}'.format(i+1, len(packages)))
-        #print(package)
 
-        robust_extract(package, client)
-    print('done!')
+
+
+new_df = pd.DataFrame(data=new_deps)
+print(new_df.head())
+
+new_df.to_csv('data/new_reqs.csv')
--- n1_fetch_all_deps/run.bat
+++ n1_fetch_all_deps/run.bat
@@ -1,1 +1,1 @@
-docker run -it --rm --mount type=bind,source=D:\Users\Nima\Documents\GitHub\fetch_pypi_dependecy\packages,target=/usr/app/packages nimamahmoudi/pypi-fetch-packages
+docker run -it --rm nimamahmoudi/pypi-fetch-dependencies-directly
